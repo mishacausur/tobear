0539ae0ddb397940d63db7a74a99a741a4cfa2af
Misha Causur <mishacausur@mail.ru>
2022-09-06 22:26:01 +0500

    t5 done

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 name = input('What is your name?\n')
 message = 'Hi, ' + name + '!'
 print(message)
+
+age = input('What is your birth year?\n')
+year = 2022 - int(age)
+message2 = 'You are ' + str(year) + ' old'
+print(message2)
