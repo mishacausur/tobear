d2b8aea6e6668f8c6950a51c2b8f67b8e7bccd3c
Misha Causur <mishacausur@mail.ru>
2022-09-07 14:18:44 +0500

    t8 done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = input('what\'s your name???\n')
-print(f'so I see, that you are {name}')
-age = input('and how old are you\n')
-print(f'{age} is a nice age I guess :)')
+text = 'I\'m studying "Python"'
+newText = text.replace('"', '\\"').replace("'", "\\'")
+
+print(newText)
