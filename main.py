914ca6ef83997399ae2598319b912436741ce49a
Misha Causur <mishacausur@mail.ru>
2022-09-12 23:52:38 +0500

    t12 done

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-text = input()
-print(len(text))
+first, second = map(int, input().split())
+a = first % 10 == 0 or second % 10 != 0
+b = first % 10 != 0 or second % 10 == 0
+print(a != b)
+print('first', a)
+print('second', b)
