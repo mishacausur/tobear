41adad1167a9e6a44cb25103d709410ebb2ad05b
Misha Causur <mishacausur@mail.ru>
2022-09-13 23:11:17 +0500

    t14 done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = input()
-reverse = text[::-1]
-print(text == reverse)
+x, y = map(int, input().split())
+w = int(x/100) + (x%10) + (x%100//10)
+i = int(y/100) + (y%10) + (y%100//10)
+
+print(w == i)
 
