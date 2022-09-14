049e125cd28f0ed31d211f5a7ec13a10fe4e226e
Misha Causur <mishacausur@mail.ru>
2022-09-14 23:52:43 +0500

    t22 done

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 num = int(input())
-print(num in numbers)
+if num in numbers:
+    print(numbers[:num-1])
+else:
+    print(numbers)
