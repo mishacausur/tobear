ec3d47c6ce0dda16fc1f7f7ba6796788d306f85b
Misha Causur <mishacausur@mail.ru>
2022-09-18 01:44:01 +0500

    t23 done

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-num = int(input())
-if num in numbers:
-    print(numbers[:num-1])
-else:
-    print(numbers)
+numbers = list(map(int, input().split()))
+if len(numbers) > 1:
+    numbers[0], numbers[-1] = numbers[-1], numbers[0]
+print(numbers)
+
