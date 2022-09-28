5ce6cc20670dd4bf27effd95457d93f338ebbd00
Misha Causur <mishacausur@mail.ru>
2022-09-28 23:53:37 +0500

    t26 done

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-number = int(input())
-value: bool
-if number > 2:
-    for item in range(2, number - 1):
-        if number % item == 0:
-            value = False
-            break
-        else:
-            value = True
-
-else:
-    value = True
-print(value)
+text = int(input())
+print(text)
