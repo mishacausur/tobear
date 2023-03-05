56cc3e5d35fd3e0895ce5e5f9ac083b7e9042c8d
Misha Causur <mishacausur@mail.ru>
2023-03-05 21:51:16 +0500

    asserts

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ def counter(element, silier):
     return (element * 2, silier * 3)
 
 assert counter(2, 3) == (4, 9)
+
+def smallest(items):
+    return min(items)
+
+assert smallest([-1, 3, 17, 21]) == -1
+assert smallest([6, 4, 2, 8]) == 2
