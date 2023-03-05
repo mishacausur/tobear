fd6465f2f96df4e27f4442175b25455170683712
Misha Causur <mishacausur@mail.ru>
2023-03-05 21:56:07 +0500

    asserts and classes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def counter(element, silier):
-    return (element * 2, silier * 3)
+class Counter:
+    def count(element, silier):
+        return  (element * 2, silier * 3)
 
-assert counter(2, 3) == (4, 9)
+    def smallest(items):
+        return min(items)
 
-def smallest(items):
-    return min(items)
-
-assert smallest([-1, 3, 17, 21]) == -1
-assert smallest([6, 4, 2, 8]) == 2
+assert Counter.count(2, 2) == (4, 6)
+assert Counter.smallest([-1, 3, 17, 21]) == -1
+assert Counter.smallest([6, 4, 2, 8]) == 2
