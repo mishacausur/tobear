1dc2ce7da290fae720c63c76942d029dc853b6fd
Misha Causur <mishacausur@mail.ru>
2023-03-07 00:06:04 +0500

    idxmax added

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('logs.csv')
-print(len(df['email'].unique()))
-print(df['source'].unique())
+hogwarts_points = pd.read_csv('hogwarts_points.csv')
+
+print(hogwarts_points)
+print()
+print(hogwarts_points['points'].sum())
+print(hogwarts_points.groupby('faculty_name')['points'].sum().sum())
+print('Winner is', hogwarts_points.groupby('faculty_name')['points'].sum().idxmax())
+
+hogwarts_points = hogwarts_points.fillna('Гриффиндор')
+print('Winner is', hogwarts_points.groupby('faculty_name')['points'].sum().idxmax())
