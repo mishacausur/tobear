1fe3db60dec56a394307ea2fa848a981d0d880f7
Misha Causur <mishacausur@mail.ru>
2023-03-06 23:29:06 +0500

    df read

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-class Counter:
-    def count(element, silier):
-        return  (element * 2, silier * 3)
+import pandas as pd
 
-    def smallest(items):
-        return min(items)
-
-assert Counter.count(2, 2) == (4, 6)
-assert Counter.smallest([-1, 3, 17, 21]) == -1
-assert Counter.smallest([6, 4, 2, 8]) == 2
+df = pd.read_csv('logs.csv')
+print(df.head())
