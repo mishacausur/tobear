c9e52c481ccd821ed78dd5f0828910c658abd6c4
Misha Causur <mishacausur@mail.ru>
2023-03-08 01:27:31 +0500

    args parameters w/ asserts

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ class Sourcer:
     def source(a, b):
         return a + b
 
+    def stringer(*args, **kwargs):
+        print('untitled args', args)
+        print('key-value args', kwargs)
+
+    def closer(x, y, z):
+        return x + y + z
+
 print(Sourcer.source(2, 3))
 
 try:
@@ -16,3 +23,11 @@ except:
     print('Can\'t source')
 
 print(Sourcer.source(*[10, 20]))
+
+Sourcer.stringer(1,2,3, key='word', key2='word2')
+
+xy = [1, 2]
+z = {'z':3}
+
+assert Sourcer.closer(*xy, **z) == 6
+
