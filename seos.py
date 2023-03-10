6717b371d92b9ae18773a27881bd2a086631dd95
Misha Causur <mishacausur@mail.ru>
2023-03-11 00:15:14 +0500

    organic sources counter

--- seos.py
+++ seos.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
 data = pd.read_excel('seo_data.xlsx', sheet_name='traffic_data')
-print(data['source'].unique())
-print(data.head())
+#print(data['source'].unique())
+#print(data.head())
 subcategory_dict = pd.read_excel('seo_data.xlsx', sheet_name='subcategory_ids')
-print(subcategory_dict.head())
+#print(subcategory_dict.head())
 category_dict = pd.read_excel('seo_data.xlsx', sheet_name='category_ids')
-print(category_dict.head())
+#print(category_dict.head())
 #data['visits'] = pd.to_numeric(data['visits'])
-print(data.loc[964])
+#print(data.loc[964])
 #print(data.groupby('source')['visits'].sum())
+
+string_count = data.loc[data['subcategory_id'] == 'total'].shape[0]
+data = data[data['subcategory_id'] != 'total']
+#print(f'Количество строк: {data.shape[0]}')
+#print(data.loc[data['subcategory_id'] == 'total'])
+data['visits'] = data['visits'].astype('int')
+result = data.groupby('source')['visits'].sum()
+print(result)
