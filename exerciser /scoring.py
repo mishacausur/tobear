5c750c722c6ffca906b0f9f9fece7e9fff250d57
Misha Causur <mishacausur@mail.ru>
2023-03-14 00:01:36 +0500

    scoring categories

--- exerciser /scoring.py
+++ exerciser /scoring.py
@@ -10,5 +10,20 @@ data = data.drop(data[(data['children'] == 20) | (data['children'] == -1)].index
 data['days_employed'] = data['days_employed'].fillna(employed_days_mean)
 data['total_income'] = data['total_income'].astype(int)
 data['education'] = data['education'].str.lower()
-print(data.duplicated().sum())
+data = data.drop_duplicates()
+
+def categorize_income(income):
+    if income <= 30000:
+        return 'E'
+    elif income <= 50000:
+        return 'D'
+    elif income <= 200000:
+        return 'C'
+    elif income <= 1000000:
+        return 'B'
+    else:
+        return 'A'
+
+data['total_income_category'] = data['total_income'].apply(categorize_income)
+print(data.head(20))
 
