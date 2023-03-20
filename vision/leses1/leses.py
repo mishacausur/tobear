663a4be430989ff66e77c21a41d7d1ef3bea3d93
Misha Causur <mishacausur@mail.ru>
2023-03-20 23:18:48 +0500

    part 3 started

--- vision/leses1/leses.py
+++ vision/leses1/leses.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
 data = pd.read_csv('visits.csv', sep='\t')
-print(data.head())
+stat = data.pivot_table(index='name', values='time_spent', aggfunc='mean')
+print(stat)
+total_visits = data['time_spent'].count()
+print(f'Количество заездов: {total_visits}')
+
+# nhl_att['avg_home_att'] = nhl_att['home_att'] / nhl_att['home_games']
+# nhl_pivot = nhl_att.pivot_table(index='season', values='avg_home_att')
