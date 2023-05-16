9bb04640c1275fa71db7553d69ae64cf6f856a23
Misha Causur <mishacausur@mail.ru>
2023-05-17 00:11:21 +0500

    machine learning teacher (get_dummies + log reg)

--- machine/first_t/les_1.py
+++ machine/first_t/les_1.py
@@ -1,19 +1,24 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 data = pd.read_csv('travel_insurance.csv')
-train, valid = train_test_split(data, test_size=.25, random_state=12345)
-features_train = train.drop('Claim', axis=1)
-target_train = train['Claim']
-features_valid = valid.drop('Claim', axis=1)
-target_valid = valid['Claim']
-print(features_train.shape)
-print(features_valid.shape)
+# train, valid = train_test_split(data, test_size=.25, random_state=12345)
+# features_train = train.drop('Claim', axis=1)
+# target_train = train['Claim']
+# features_valid = valid.drop('Claim', axis=1)
+# target_valid = valid['Claim']
+# print(features_train.shape)
+# print(features_valid.shape)
 print(data.dtypes)
 
 data_ohe = pd.get_dummies(data, drop_first=True)
 target = data_ohe['Claim']
 features = data_ohe.drop('Claim', axis=1)
+
+features_train, target_train, features_valid, target_valid = train_test_split(features, target, test_size=.25, random_state=12345)
+model = LogisticRegression(random_state=12345, solver='liblinear')
+model.fit(features_train, features_valid)
 
 
 
