57d7a37ca735234a1148ed8fd84eb8163800eaf7
Misha Causur <mishacausur@mail.ru>
2023-05-19 23:36:26 +0500

    machine learning teacher (martix)

--- machine/first_t/les_2_matrix_mistakes.py
+++ machine/first_t/les_2_matrix_mistakes.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
 
 data = pd.read_csv('travel_insurance_preprocessed.csv')
 
@@ -15,3 +18,14 @@ model.fit(features_train, target_train)
 predicted_valid = model.predict(features_valid)
 
 print(confusion_matrix(target_valid, predicted_valid))
+
+print(recall_score(target_valid, predicted_valid))
+print(precision_score(target_valid, predicted_valid))
+print(f1_score(target_valid, predicted_valid))
+
+target_i = pd.Series([1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1])
+predictions_i = pd.Series([1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1])
+
+precision = precision_score(target_i, predictions_i)
+recall = recall_score(target_i, predictions_i)
+f1 = (2 * precision * recall) / (precision + recall)
