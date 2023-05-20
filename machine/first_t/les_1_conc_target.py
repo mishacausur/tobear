83ed27d5fb4eb74006043b26d35e86d69328313b
Misha Causur <mishacausur@mail.ru>
2023-05-20 23:35:11 +0500

    machine learning teacher (target count upscaling)

--- machine/first_t/les_1_conc_target.py
+++ machine/first_t/les_1_conc_target.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 data = pd.read_csv('travel_insurance_preprocessed.csv')
 
@@ -8,14 +9,18 @@ features = data.drop('Claim', axis=1)
 features_train, features_valid, target_train, target_valid = train_test_split(
     features, target, test_size=0.25, random_state=12345)
 
-features_zeros = features_train[target_train == 0]
-features_ones = features_train[target_train == 1]
-target_zeros = target_train[target_train == 0]
-target_ones = target_train[target_train == 1]
+def upsample(features, target, repeat):
+    features_zeros = features[target == 0]
+    features_ones = features[target == 1]
+    target_zeros = target[target == 0]
+    target_ones = target[target == 1]
+    features_upsampled = pd.concat([features_zeros] + [features_ones] * repeat)
+    target_upsampled = pd.concat([target_zeros] + [target_ones] * repeat)
+    return features_upsampled, target_upsampled
 
-repeat = 10
-features_upsampled = pd.concat([features_zeros] + [features_ones] * repeat)
-target_upsampled = pd.concat([target_zeros] + [target_ones] * repeat)
+shuffle(features_train, target_train, random_state=12345)
+
+features_upsampled, target_upsampled = upsample(features_train, target_train, 10)
 
 print(features_upsampled.shape)
 print(target_upsampled.shape)
